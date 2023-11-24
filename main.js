8fbb7356b488ceb3de04193d5c28dc3dd0291fef
Gustavo Caroni <125035017+Gustavombfc@users.noreply.github.com>
2023-11-24 07:22:21 -0300

    45

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-alert("Todas as Imagens quando clicadas redirecinaram para um VIDEO")
+alert("Todas as Imagens quando clicadas redirecinaram para um VIDEO Trabalho feito por Gustavo Caroni Gustavo Gomes Victor Hugo")
